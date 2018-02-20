2a90ecd621c45356b699975e909b9ce5dd363a09
Andrea Mangano <andrea.mangano@ibuildings.it>
2018-02-21 00:09:27 +0100

    Aligns style with other components

--- src/components/Pagination/style.js
+++ src/components/Pagination/style.js
@@ -8,29 +8,32 @@ const { palettes } = theme;
 const { basic, neutral } = palettes;
 
 export const base = css`
-  display: flex;
+  display: inline-flex;
   align-items: center;
+  box-shadow: 0 2px 10px 0 rgba(0,0,0,0.05);
 `;
 
 
 export const button = css`
   ${typography.body}
   ${size(extRem(34),extRem(34))}
-  border: 1px solid ${basic.primary};
+  border: 1px solid ${neutral.medium};
   outline: 0;
   background: none;
   margin-left: -1px;
   display: inline-flex;
   align-items: center;
   justify-content: center;
-  color: ${basic.primary};
+  color: ${neutral.highest};
   padding: 0;
   cursor: pointer;
   
   ${props => props.active && `
     background-color: ${basic.primary};
     color: ${neutral.lowest};
-    
+    border: 1px solid ${basic.primary};
+    position: relative;
+    z-index: 1;
   `}
   
   ${props => props.disabled && `
