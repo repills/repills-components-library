25fd62ac6d544ef36f98ca129e6e7940e3c224f1
Andrea Mangano <andrea.mangano@ibuildings.it>
2018-02-20 23:21:05 +0100

    Adds z-index

---
+++ src/config/zIndex.js
@@ -0,0 +1,12 @@
+export default {
+  overthetop: 99999,
+  modal: 7000,
+  sidebar: 6000,
+  navbar: 5000,
+  popover: 4000,
+  overlay: 3000,
+  inpage: 1000,
+  standard: 1,
+  below: -1,
+  bottomless: -99999
+};
