f7055d22f3ed9d84d005da7d5be62a74f8351a00
Andrea Mangano <andrea.mangano@ibuildings.it>
2018-02-27 19:25:21 +0100

    Creates component for the home page header

---
+++ doc/stories/HomePageHeader/basic.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { text } from '@storybook/addon-knobs/react';
+import { HomePageHeader } from '../../../src/components';
+import { SectionPage, InteractiveExample } from '../../components';
+
+export default (sectionTitle, sectionDescription) => () => {
+
+  const title = text('Title', 'Free pills around the web');
+  const subTitle = text('SubTitle', 'for web developers and UI designers');
+  const description = text('Description', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium, massa a varius efficitur, orci dui semper ex, sit amet porta sem dui ac odio.');
+
+  return (
+    <SectionPage
+      sectionDescription={sectionDescription}
+      subSectionTitle="Basic"
+      title={sectionTitle}
+    >
+      <InteractiveExample
+        component={HomePageHeader}
+        description={description}
+        subTitle={subTitle}
+        title={title}
+      />
+    </SectionPage>
+  );
+};
