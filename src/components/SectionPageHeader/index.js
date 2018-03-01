83d8d9ae56257aabab48521f2e85e66e526c3ac2
Andrea Mangano <andrea.mangano@ibuildings.it>
2018-03-01 20:11:51 +0100

    Addes responsiveness to Section Header

--- src/components/SectionPageHeader/index.js
+++ src/components/SectionPageHeader/index.js
@@ -3,17 +3,26 @@ import {
   string
 } from 'prop-types';
 import * as sectionsIcons from '../Icon/icons/sections';
+import { ContainerQuery } from 'react-container-query';
+import { getCurrentBreakpoint } from '../../utils';
+import { MD, query } from '../../config/breakpoints';
 import styled from 'styled-components';
 import {
   base,
   icon,
+  label,
   title,
+  titleContainer,
+  main,
   description
 } from './style';
 
 const BaseStyle = styled.div`${base}`;
+const LabelStyle = styled.div`${label}`;
 const IconStyle = styled.div`${icon}`;
 const TitleStyle = styled.h1`${title}`;
+const MainStyle = styled.div`${main}`;
+const TitleContainerStyle = styled.div`${titleContainer}`;
 const DescriptionStyle = styled.p`${description}`;
 
 const SectionPageHeader = ({
@@ -26,21 +35,35 @@ const SectionPageHeader = ({
   const Icon = sectionsIcons[icon];
 
   return (
-    <BaseStyle>
+    <ContainerQuery query={query}>
       {
-        Icon &&
-        <IconStyle
-          color={color}
-        >
-          <Icon size={120} />
-        </IconStyle>
+        params => (
+          <BaseStyle
+            breakpointsStatus={params}
+          >
+            <MainStyle
+              breakpointsStatus={params}
+            >
+              {
+                Icon &&
+                <IconStyle
+                  breakpointsStatus={params}
+                  color={color}
+                >
+                  <Icon size={params[MD] ? 80 : 100} />
+                </IconStyle>
+              }
+              <LabelStyle>Section</LabelStyle>
+              <TitleStyle>{title}</TitleStyle>
+            </MainStyle>
+            {
+              description &&
+              <DescriptionStyle icon={icon}>{description}</DescriptionStyle>
+            }
+          </BaseStyle>
+        )
       }
-      <TitleStyle>{title}</TitleStyle>
-      {
-        description &&
-        <DescriptionStyle>{description}</DescriptionStyle>
-      }
-    </BaseStyle>
+    </ContainerQuery>
   );
 };
 
--- src/components/SectionPageHeader/style.js
+++ src/components/SectionPageHeader/style.js
@@ -1,30 +1,68 @@
 import { css } from 'styled-components';
 import { extRem } from '../../utils';
+import { ellipsis } from 'polished';
 import theme from '../../config/theme';
 import typography from '../../shared/styles/typography';
+import { grid } from 'styled-components-grid';
+import { SM, MD, LG } from '../../config/breakpoints';
 
 const { basic, types, neutral } = theme.palettes;
 
 export const base = css`
-  padding: ${extRem(60)} 0;
+  padding: ${extRem(40)} 0;
   text-align: center;
+  
+  ${props => props.breakpointsStatus[MD] && `
+    text-align: left;
+    padding: ${extRem(60)} 0;
+  `}
 `;
 
 export const title = css`
-  ${typography.header1}
+  ${typography.header2}
   color: ${neutral.highest};
   margin: 0;
   line-height: 1.1;
+  word-break: break-word;
+  margin-left: ${extRem(-3)};
+`;
+
+// ${typography.header1}
+
+export const main = css`
+  position: relative;
+    
+  ${props => props.breakpointsStatus[MD] && `
+    display: inline-block;
+    padding-right: ${extRem(100)};
+  `}
+`;
+
+export const titleContainer = css`
+
+`;
+
+export const label = css`
+  ${typography.body}
+  color: ${neutral.mediumHigh};
+  text-transform: uppercase;
 `;
 
 export const icon = css`
   color: ${props => props.color};
+  margin-bottom: ${extRem(12)};
+  
+  ${props => props.breakpointsStatus[MD] && `
+    position: absolute;
+    right: 0;
+    top: 0;
+  `}
 `;
 
 export const description = css`
   ${typography.body}
   color: ${neutral.highest};
+  margin: ${extRem(24)} 0 0;
   line-height: 1.5;
   max-width: ${extRem(500)};
-  margin: ${extRem(40)} auto 0;
 `;
