c895f5351681d3b398e0267f889fd24a67b7d940
Andrea Mangano <andrea.mangano@ibuildings.it>
2018-03-06 22:50:10 +0100

    Adds future components scaffolding

---
+++ src/components/ContributorsLeaderboard/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  node,
+  number,
+  oneOf
+} from 'prop-types';
+import styled from 'styled-components';
+import { base } from './style';
+
+const BaseStyle = styled.div`${base}`;
+
+const ContributorsLeaderboard = ({
+
+}) => {
+
+  return (
+    <BaseStyle>
+      TODO
+    </BaseStyle>
+  );
+};
+
+ContributorsLeaderboard.propTypes = {
+
+};
+
+ContributorsLeaderboard.defaultProps = {
+};
+
+export default ContributorsLeaderboard;
---
+++ src/components/MaintainersList/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  node,
+  number,
+  oneOf
+} from 'prop-types';
+import styled from 'styled-components';
+import { base } from './style';
+
+const BaseStyle = styled.div`${base}`;
+
+const MaintainersList = ({
+
+}) => {
+
+  return (
+    <BaseStyle>
+      TODO
+    </BaseStyle>
+  );
+};
+
+MaintainersList.propTypes = {
+
+};
+
+MaintainersList.defaultProps = {
+};
+
+export default MaintainersList;
