d0803ed5c4279fc3241b5132f1b2030b3046effd
Andrea Mangano <andrea.mangano@ibuildings.it>
2018-03-08 00:35:27 +0100

    Replaces old icon

--- src/components/ResourceDetail/index.js
+++ src/components/ResourceDetail/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import m from 'moment';
 import { getBaseUrl } from '../../utils';
-import { SquarePillIcon, LinkIcon } from '../Icon/icons/basic';
+import { SquareFilledPillIcon, LinkIcon } from '../Icon/icons/basic';
 import Button from '../Button';
 import {
   string,
@@ -59,7 +59,7 @@ function ResourceDetail({
       <MainInfoStyle>
         <MainInfoTopStyle>
           <TypeStyle>
-            <SquarePillIcon
+            <SquareFilledPillIcon
               color={color}
               size={16}
             />
